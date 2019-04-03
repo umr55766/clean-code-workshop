32dac35422d2f8d820938e8174f8a53b5566870a
Umair <umair.mohammad@thoughtworks.com>
2019-04-03 14:26:09 +0530

    Refactor out calculateAmountFor method to Rental class as getRentAmount

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -26,7 +26,7 @@ public class Customer {
     String result = "Rental Record for " + getName() + "\n";
 
     for (Rental rental : rentals) {
-      double thisAmount = calculateAmountFor(rental);
+      double thisAmount = rental.getRentAmount();
 
       // add frequent renter points
       frequentRenterPoints++;
@@ -47,31 +47,6 @@ public class Customer {
     result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
     return result;
-  }
-
-  private double calculateAmountFor(Rental each) {
-    double thisAmount = 0;
-    //determine amounts for each line
-    switch (each.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-        thisAmount += 2;
-
-        if (each.getDaysRented() > 2)
-          thisAmount += (each.getDaysRented() - 2) * 1.5;
-        break;
-
-      case Movie.NEW_RELEASE:
-        thisAmount += each.getDaysRented() * 3;
-        break;
-
-      case Movie.CHILDRENS:
-        thisAmount += 1.5;
-
-        if (each.getDaysRented() > 3)
-          thisAmount += (each.getDaysRented() - 3) * 1.5;
-        break;
-    }
-    return thisAmount;
   }
 }
 
